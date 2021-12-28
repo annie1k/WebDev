910534b527faa77c1b38eec9b8b5242a2e81854f
Annie <1000wangyiqian@gmail.com>
2021-12-28 08:12:11 -0800

    drum

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,9 +1,43 @@
+var crashK = new Audio('sounds/crash.mp3');
+var kickL = new Audio('sounds/kick-bass.mp3');
+var snareJ = new Audio('sounds/snare.mp3');
+var tom1W = new Audio('sounds/tom-1.mp3');
+var tom2A = new Audio('sounds/tom-2.mp3');
+var tom3S = new Audio('sounds/tom-3.mp3');
+var tom4D = new Audio('sounds/tom-4.mp3');
+
+
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
 }
 
 
 function handleClick() {
-    alert("I got clicked!");
+    switch(this.innerHTML) {
+        case "w":
+            tom1W.play();
+            break;
+        case "a":
+            tom2A.play();
+            break;
+        case "s":
+            tom3S.play();
+            break;
+        case "d":
+            tom4D.play();
+            break;
+        case "j":
+            snareJ.play();
+            break;
+        case "k":
+            crashK.play();
+            break;
+        case "l":
+            kickL.play();
+            break;
+        default:
+            console.log("wrong")
+    }
 }
+
 
