a44fa7c16563eda8aba9381d7ea0715d2a0d16f1
Annie <1000wangyiqian@gmail.com>
2022-01-03 15:42:22 -0800

    step 3 for simon game

--- SimonGame/game.js
+++ SimonGame/game.js
@@ -1,8 +1,16 @@
 var buttonColor = ["red","blue","green","yellow"];
 var gamePattern = [];
+var blue = new Audio('sounds/blue.mp3');
+var green = new Audio('sounds/green.mp3');
+var red = new Audio('sounds/red.mp3');
+var yellow = new Audio('sounds/yellow.mp3');
+var wrong = new Audio('sounds/wrong.mp3');
 
 function nextSequence(){
     var randNum = Math.floor(Math.random() * 4);
     var randColor = buttonColor[randNum];
     gamePattern.push(randColor);
+    $("#" + randColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("sounds/" + randColor + ".mp3");
+    audio.play();
 }
