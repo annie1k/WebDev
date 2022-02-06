f7659c52b122502619126fad04a9759f76622b9f
Annie <1000wangyiqian@gmail.com>
2022-02-05 22:13:30 -0800

    ts start

---
+++ TS-quick-tut/tutorial.ts
@@ -0,0 +1,9 @@
+//typescript requires data type automatically
+var firstName: string = "Annie";
+const increment = (counterVal: number) => {
+    return counterVal + 1;
+}
+
+const counter: number = increment(77);
+
+let programmingLanguages: string[] = ["Java", "Python", "JavaScript"];
